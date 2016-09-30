fcc1dc27500daf30f8ae446455255a7c679a0089
eliotex <nathann-s@outlook.com>
2016-09-30 19:12:04 -0300

    Mudanças em algumas diretrizes do projeto

--- classe.py
+++ classe.py
@@ -1,22 +1,36 @@
 import _md5
+import random
 class neero:
-    def __init__(self,mensagem='',chave=0,dic=[],dic2=[]):
+    def __init__(self,mensagem='',dic=[],dic2=[]):
         self.mensagem = mensagem
+    def cript_caesar(self,chave):
         self.chave = chave
-        self.dic = dic
-        self.dic2 = dic2
-    def cript_caesar(self):
-        for letter in str(self.mensagem):
-            print(self.dic[self.dic.index(letter)+int(self.chave)],end="")
-    def decrypt_caesar(self):
         for letter in self.mensagem:
-            print(self.dic[self.dic.index(letter)-int(self.chave)])
+            n = ord(letter) + int(self.chave)
+            if letter.isspace():
+                print(letter,end="")
+            else:
+                print(chr(n),end="")
+        print("\n")
+    def decrypt_caesar(self,chave):
+        self.chave=chave
+        for letter in self.mensagem:
+            if letter.isspace():
+                print(letter,end="")
+            else:
+                print(chr(ord(letter) - int(self.chave)),end="")
+        print("\n")
+
     def cript_vigenere(self):
         for letter in self.mensagem:
             print(self.dic2[self.dic.index(letter)])
+
     def decrypt_vigenere(self):
         for letter in self.mensagem:
             print(self.dic[self.dic2[self.dic.index(letter)]])
-    def cript_md5(self,_md5):
+
+    def cript_md5(self, _md5):
         m = _md5.new()
         m.md5.new(self.mensagem).digest()
+
+
--- neerocripter.py
+++ neerocripter.py
@@ -3,34 +3,33 @@ def Main():
     print(arg.help)
 import classe
 import argparse
-dic = ["a","b","c","d",'e','f''g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-dic2 = []
 menu = argparse.ArgumentParser("A cripto tool")
-menu.add_argument("--cc","--cript_caesar",help="Caesar Cipher",action="store")
-menu.add_argument("--key","--kk",help="Argument for chipher were needs keys",action="store")
-menu.add_argument("--dc","--dca",help="Decrypt Caesar Chipher",action="store")
-menu.add_argument("--caesar", "--caesar", help="Option to cript and decript caesar ciphers", action="store_true",default="false")
-menu.add_argument("--vig","--vig",help="Vigenere cypher",action="store_true",default="false")
-menu.add_argument("--cvig","--cvig",help="Cript Vigenere Cypher",action="store")
-menu.add_argument("--dvig","--dvig",help="Decrypt Vigenere Cypher",action="store")
-menu.add_argument("--md5","--md5",help="md5 function",action="store_true",default="false")
-menu.add_argument("--cmd5","--cmd5",help="cript md5",action="store")
+menu.add_argument('--fc','--fc',action="store_true",default="false")
+menu.add_argument('-sc','-sc',action="store_true",default='false')
+menu.add_argument('-k','-key',action="store",type=int)
+menu.add_argument('-dc','-dc',action="store")
+menu.add_argument('--md','--md',action="store_true",default='false')
+menu.add_argument('--fv','-fv',action="store_true",default='false')
+menu.add_argument('--sv','-sv',action="store_true",default='false')
+menu.add_argument('--vdc','-vdc',action="store_true",default="false")
+menu.add_argument('-cipher','-cipher',action="store")
+menu.add_argument('-md5','-md5',action="store")
 arg = menu.parse_args()
 
-if arg.caesar == True:
-    c = classe.neero(arg.cc,arg.key,dic)
-    print(c.cript_caesar())
-    print(c.decrypt_caesar())
-if arg.vig == True:
-    c = classe.neero(arg.cvig,arg.dic,arg.d)
-    print("Criptografia vig")
-    elementos = input("Digite os número de elementos do dicionario:")
-    if elementos.isalpha:
-        print("Você colocou uma letra por favor digite um número")
-    else:
-        for number in int(elementos):
-            dados = input("Digite os dados do dicionario")
-            dic2.insert(dados)
-    print(c.cript_vigenere())
-    print(c.decrypt_vigenere())
-print(c.cript_md5())
+if arg.fc == True:
+    mensagem = input("Digite sua mensagem:")
+    cript = classe.neero(mensagem)
+    cript.cript_caesar(arg.k)
+if arg.sc == True:
+    decrypt = classe.neero(arg.dc)
+    cript.decrypt_caesar(arg.k)
+if arg.fv == True:
+    mensagem = input("Digite sua mensagem:")
+    cript = classe.neero(mensagem)
+    cript.cript_vigenere(arg.cipher)
+if arg.sv == True:
+    cript = classe.neero(arg.vdc)
+    cript.decrypt_caesar(arg.cipher)
+if arg.md == True:
+    cript = classe.neero(arg.md5)
+    print(cript.cript_md5())
